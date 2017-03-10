015c7b79769c9bdefafa3882fc0e3e5498c90d0d
bdszcz@X201 <bdszcz@163.com>
2017-03-10 22:52:32 +0800

    黑金FPGA平台测试通过，硬件crc结果和python zlib结果一致

--- rw_uart_python/RW_UART.py
+++ rw_uart_python/RW_UART.py
@@ -1,21 +1,25 @@
 import serial
 import time
+import zlib
 ser = serial.Serial()
 ser.port = "COM5"
 ser.baudrate = 115200
 ser.open()
+
 i = 0
 crc = 0
-start_time = time.time()
-loop = 1000 * 100
+loop = 1000 * 10
 bytes_per_loop = 10
+data = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09'
+
+start_time = time.time()
 while i < loop:
-    ser.write(b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09')
-    crc = (crc + 45) % 256
+    ser.write(data)
+    crc = zlib.crc32(data, crc)
     i = i + 1
 end_time = time.time()
 print "elapse time  : %8.2f" % (end_time - start_time)
-print "total bytes  : %8d" % (bytes_per_loop * loop)
+print "total bytes  : %8d (0x%X)" % (bytes_per_loop * loop, bytes_per_loop * loop )
 print "transfer rate: %8d bps" % (bytes_per_loop * loop * 8 / (end_time - start_time))
 print "crc          : %8x" % crc
 ser.close()
